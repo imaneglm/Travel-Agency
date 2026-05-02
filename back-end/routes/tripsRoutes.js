6daf8cb686fc4ef3250eb7777bbb382a28a22976
imaneglm <209016594+imaneglm@users.noreply.github.com>
2026-05-02 11:35:13 +0100

    Delete back-end directory

--- back-end/routes/tripsRoutes.js
+++
@@ -1,8 +0,0 @@
-
-const express = require("express");
-const router = express.Router();
-const { getAllTrips } = require("../controllers/tripsController");
-
-router.get("/", getAllTrips); // GET /api/trips
-
-module.exports = router;
